032ee8ef1dc7c1e39ac87a8e3a0fa1dc8b20cf48
Elizabeth <asobingai@gmail.com>
2019-09-09 06:13:37 +0100

    patient care plan view

--- interface/forms/patient_care_plan/view.php
+++ interface/forms/patient_care_plan/view.php
@@ -28,17 +28,18 @@ $returnurl = 'encounter_top.php';
 	<body>
 		<?php
 			include_once("$srcdir/api.inc");
-			$res2 = sqlStatement("SELECT MAX(id) as largestId FROM `form_patient_care_plan`");
+			$res2 = sqlStatement("SELECT count(*) AS numOfRows FROM `form_patient_care_plan` WHERE id =?", array($_GET["id"]));
     		$getMaxid = sqlFetchArray($res2);
-			$obj = formFetch("form_patient_care_plan", $_GET["id"]);
+    		$sql = "SELECT * FROM `form_patient_care_plan` WHERE id=? AND pid = ? AND encounter = ?";
+			$obj = sqlStatement($sql, array($_GET["id"],$_SESSION["pid"], $_SESSION["encounter"]));
 
-			if ($getMaxid['largestId']) {
-				$count = $getMaxid['largestId'] + 1 ;
+			if ($getMaxid['numOfRows']) {
+				$count = $getMaxid['numOfRows'] + 1 ;
 				$html = "<input type=\"hidden\" id=\"num\" value=" . $count . " />";
 				echo $html;
 		        
 		    } else {
-		        // echo "<input type=\"hidden\" id=\"num\" value=\"1\" />";
+		        echo "<input type=\"hidden\" id=\"num\" value=\"1\" />";
     		}
 		?>
 		<form class="form-horizontal" method=post action="<?php echo $rootdir;?>/forms/patient_care_plan/save.php?mode=update&id=<?php echo attr($_GET["id"]);?>" name="my_form" onsubmit="return top.restoreSession()">
@@ -47,6 +48,80 @@ $returnurl = 'encounter_top.php';
 				<input type="submit" id="save" class='btn btn-success' value="<?php echo xla('Save'); ?>"> &nbsp;
 				<input type="button" id="dontsave" class="deleter btn btn-danger" value="<?php echo xla('Cancel'); ?>"> &nbsp;
 			</div>
+
+			<table style="width:100%">
+				<thead>
+					<tr>
+					    <th>Issue #</th>
+					    <th>Key Issue <span><a href="#" ><span title="add issue" onclick="addIssue()" style="color: #337ab7" class="glyphicon glyphicon-plus-sign"></span></a></span></th> 
+			 	 	</tr>
+				</thead>
+				<tbody class="addIssue">
+				<?php 
+					for ($iter = 0; $row = sqlFetchArray($obj); $iter++){
+							$data[$iter] = $row;
+							// echo "<p>".json_encode($data[0])."</p>";
+							// echo "<p>".json_encode($_GET["id"])."</p>";
+							// echo "<p>".$iter."</p>";
+
+					}	
+			    	if ($data) {
+			    		$j = 1;
+			    		foreach ($data as $key => $value) {
+			    		
+			    ?>
+
+			    		<tr <?php echo "id=row_".$j;?> >
+						  	<td>
+						  		<mark><?php echo text($value['issue'] + 1); ?></mark>
+						  		<a href="#" ><span style="color: #337ab7" onclick="addIntervention(<?php echo text($value['issue'] + 1); ?>)" class="glyphicon glyphicon-plus-sign" title="Add Interventions"></span> </a>
+						  		<a href="#"><span style="color: red" onclick="deactivateIssue(<?php echo text($value['issue'] + 1); ?>)" class="glyphicon glyphicon-ban-circle" title="Deactivate Issue"></span> </a> 
+						  	</td>
+						  	<td><input type="text" name="issue[]" placeholder="key issue" value="<?php echo text($value['Key_issue']); ?>"></td>
+						  	<input type="hidden" class="status_<?php echo text($value['status']); ?>" name="status[]" value="<?php echo text($value['status']); ?>">
+						  	<input type="hidden"  name="count[]" value="<?php echo text($value['issue'] + 1); ?>">
+						  	<td>
+						      <table>
+						      	<thead>
+						      		<tr>
+									   <th>Intervention</th>
+									   <th>Outcome</th> 
+									   <th>Goal</th>
+									   <th>Progress</th>
+									</tr>
+						      	</thead>
+						      	<tbody class="intervention_<?php echo text($value['issue'] + 1); ?>">
+						      		<tr>
+						      			<?php
+				                            $intervention =  json_decode($value['Interventions']);
+				                            $outcome =  json_decode($value['Outcome']);
+				                            $goal =  json_decode($value['Goal']);
+				                            $progress_ =  json_decode($value['Progress']);
+				                            for($i = 0; $i < count($intervention); $i++) {
+				                         ?>		
+				                         		<td><textarea name="intervention_<?php echo $i; ?>[]"><?php echo text($intervention[$i]);?></textarea></td>
+										        <td><textarea name="outcome_<?php echo $i; ?>[]"><?php echo text($outcome[$i]);?></textarea></td>
+										        <td><textarea name="goal_<?php echo $i; ?>[]"><?php echo text($goal[$i]);?></textarea></td>
+										        <td><textarea name="progress_<?php echo $i; ?>[]"><?php echo text($progress[$i]);?></textarea></td> 
+										         </tr>
+										<?php
+										}		                            
+				                        ?>
+						      	</tbody>   
+						      </table>
+						    </td>    
+					  	</tr>
+			    <?php
+			    		 $j = $j + 1;
+				    		}
+			        	}
+				?>
+				</tbody>
+			</table>
+			<textarea hidden="hidden"   name="Interventions"></textarea>
+			<textarea hidden="hidden" name="Outcome"></textarea>
+			<textarea hidden="hidden" name="Goal"></textarea>
+			<textarea hidden="hidden"  name="Progress" ></textarea>
 		</form>
 
 		<script type="text/javascript">
@@ -63,6 +138,7 @@ $returnurl = 'encounter_top.php';
 
 		}
 		function deactivateIssue(issueNumber) {
+			console.log(issueNumber);
 			$(`.status_${issueNumber}`).attr("value", "0");
 			$(`#row_${issueNumber}`).css("background-color", "#c5c5bc");
 			$(`#row_${issueNumber}`).find("input, textarea").attr("readonly","readonly");
